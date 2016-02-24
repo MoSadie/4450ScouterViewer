1afbfa9c2dc1da33343e4d381904ee018dffbba9
ShortCircuit908 <P_Ir_At_Es@comcast.net>
2016-02-24 10:33:28 -0800

    Definitely does it

--- index.php
+++ index.php
@@ -10,18 +10,18 @@ if (isset($_FILES['userfile'])) {
 		$file_tmp = $_FILES['userfile']['tmp_name'][$i];
 		$file_type = strtolower($_FILES['userfile']['type'][$i]);
 		if ($file_type != "image/jpeg" || !preg_match("/ROBOT_\\d+$/", $file_name)) {
-			$failed .= "$file_name, ";
+			$failed .= "$file_name\\n";
 			continue;
 		}
 		$file_name_no_ext = pathinfo($file_name, PATHINFO_FILENAME);
 		if (!move_uploaded_file($file_tmp, "uploaded/images/$file_name_no_ext.JPG")) {
-			$failed .= "$file_name, ";
+			$failed .= "$file_name\\n";
 		}
 	}
 	if (!empty($failed)) {
 		?>
 		<script type="text/javascript">
-			alert("Failed to upload files:<?php echo $failed?>");
+			alert("Failed to upload files:\n<?php echo $failed?>");
 		</script>
 		<?php
 	}
