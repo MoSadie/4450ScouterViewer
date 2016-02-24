f57aa1e7dcaed2f0d394e9a7d8b683face849a67
ShortCircuit908 <P_Ir_At_Es@comcast.net>
2016-02-24 11:26:29 -0800

    Check response state

--- js/scouting.js
+++ js/scouting.js
@@ -16,24 +16,28 @@ function getScores(){
     request.open("GET", "scoutdata.php?action=getinfo&team=" + team_number.value.trim(), true);
     request.send();
     request.onreadystatechange = function () {
-        var response = request.responseText;
-        console.log(response);
-        var matches = JSON.parse(response);
-        $("average_score").innerHTML = "<span>Average score: </span>" + matches["average"];
-        $("reliability").innerHTML = "<span>Reliability: </span>" + matches["variance"];
-        $("team_name").innerHTML = "<span>Team Name: </span>" + matches["team_name"];
-        $("robot_description").innerHTML = "<h4>Robot description: </h4>" + matches["robot_description"];
-        $("robot_image").attr("src", matches["image"]);
-        var list = "";
-        matches["auto_behavior"].forEach(function(entry){
-            list += "<li value=\"" + entry["match_number"] + "\">" + entry["autonomous_behavior"] + "</li>";
-        });
-        document.getElementById("autonomous_behavior").innerHTML = list;
+        if (request.readyState == 4 && request.status == 200) {
+            var response = request.responseText;
+            console.log(response);
+            var matches = JSON.parse(response);
+            $("average_score").innerHTML = "<span>Average score: </span>" + matches["average"];
+            $("reliability").innerHTML = "<span>Reliability: </span>" + matches["variance"];
+            $("team_name").innerHTML = "<span>Team Name: </span>" + matches["team_name"];
+            $("robot_description").innerHTML = "<h4>Robot description: </h4>" + matches["robot_description"];
+            $("robot_image").attr("src", matches["image"]);
+            var list = "";
+            matches["auto_behavior"].forEach(function (entry) {
+                list += "<li value=\"" + entry["match_number"] + "\">" + entry["autonomous_behavior"] + "</li>";
+            });
+            document.getElementById("autonomous_behavior").innerHTML = list;
+        }
     };
     var match_request = new XMLHttpRequest();
     match_request.open("GET", "scoutdata.php?action=getmatches&team=" + team_number.value.trim(), true);
     match_request.send();
     match_request.onreadystatechange = function () {
-        document.getElementById("match_data").innerHTML = match_request.responseText;
+        if (request.readyState == 4 && request.status == 200) {
+            document.getElementById("match_data").innerHTML = match_request.responseText;
+        }
     }
 }
