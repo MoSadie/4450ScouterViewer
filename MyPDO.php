ba64c12d46846631a63ac23ef5664748d1f863bb
ShortCircuit908 <P_Ir_At_Es@comcast.net>
2016-03-01 14:12:18 -0800

    A thing oy vey

--- MyPDO.php
+++ MyPDO.php
@@ -5,6 +5,13 @@
  */
 class MyPDO extends PDO {
 	public function __construct() {
-		parent::__construct("mysql:host=localhost;dbname=orf;", "milligancn", "9390164", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		try {
+			parent::__construct("mysql:host=localhost;dbname=database;", "username", "password", array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		}
+		catch(Throwable $e){
+			error_log($e->__toString());
+			header("Location: errpage.php?timestamp=" . time() . "&err=" . get_class($e), true);
+			exit();
+		}
 	}
 }
